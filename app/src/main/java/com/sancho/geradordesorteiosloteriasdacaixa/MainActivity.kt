7b679d8b938bb1f69294bf5fa6b7b2a3e503c9e5
Francisco Sancho <frsancho1985@gmail.com>
2023-12-07 12:41:25 -0300

    Add new screen and button navigation

--- app/src/main/java/com/sancho/geradordesorteiosloteriasdacaixa/MainActivity.kt
+++ app/src/main/java/com/sancho/geradordesorteiosloteriasdacaixa/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.sancho.geradordesorteiosloteriasdacaixa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnMegaDaVirada: Button = findViewById(R.id.btn_mega_da_virada)
+        val btnMegaSena: Button = findViewById(R.id.btn_mega_sena)
+        val btnQuina: Button = findViewById(R.id.btn_quina)
+
+        btnMegaDaVirada.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
     }
 }
